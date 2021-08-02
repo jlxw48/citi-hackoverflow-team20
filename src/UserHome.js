016f5ed250647aa2a1452990ba9321a05178bdc3
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2021-08-02 21:10:10 +0800

    Trying to pull firebase data

--- src/UserHome.js
+++ src/UserHome.js
@@ -1,13 +1,19 @@
 import {database} from "./firebase.js"
 
+var userRef = database.collection("user").doc("tom@gmail.com")
 function UserHome() {
-  const response = database.collection("user/");
+  userRef.get().then((doc) => {
+    if (doc.exists) {
+      console.log(doc.data)
+    }
+
+  })
   return (
     <div>
       <h2>HOMEEEE</h2>
         <ul>
           <li>
-            {response.name}
+
           </li>
         </ul>
     </div>
