c65259d8576d5f31121aaabdb14b8442f38f0331
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2021-08-02 22:10:27 +0800

    Pull data from firebase to UserHome

--- src/UserHome.js
+++ src/UserHome.js
@@ -1,23 +1,44 @@
 import {database} from "./firebase.js"
+import React, { useState, useEffect } from 'react';
 
-var userRef = database.collection("user").doc("tom@gmail.com")
 function UserHome() {
-  userRef.get().then((doc) => {
-    if (doc.exists) {
-      console.log(doc.data)
-    }
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  })
+  const ref = database.collection("user")
+
+  function getUsers() {
+    setLoading(true);
+    ref.get().then((item) => {
+      const items = item.docs.map((doc) => doc.data());
+      setUsers(items);
+      setLoading(false);
+      console.log(items)
+    });
+  }
+  useEffect(() => {
+    getUsers();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
       <h2>HOMEEEE</h2>
         <ul>
           <li>
-
+          {loading ? <h1>Loading...</h1> : null}
+          {users.map((user) => (
+            <div key={user.email}>
+              <h2>{user.email}</h2>
+              <p>{user.loyalty}</p>
+              <p>{user.name}</p>
+              </div>
+          ))}
           </li>
         </ul>
     </div>
   )
 }
+
 
 export default UserHome;
